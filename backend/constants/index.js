24979ed46c9a1a50287b22e8f624b486e5b8ed83
Deepak2030 <deepakshah9016@gmail.com>
2023-06-17 07:50:39 +0530

    Initial commit

---
+++ backend/constants/index.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x65c42d8f56498A3f03fEe4187b910445D3fe96dd";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
